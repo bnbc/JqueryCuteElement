889acc5bbd4f6a5e631cc92d2e206d0fbb254c08
bnbc <benjamin.beyssac@gmail.com>
2015-06-10 23:30:32 +0200

    jQuery Cute Element v1.0

---
+++ README.md
@@ -0,0 +1,114 @@
+# jQuery Cute Element
+Ce plugin jQuery vous permettra de donner du style à vos élements select, radio et checkbox.  
+
+## Utilisation
+
+### CSS
+Importer le fichier dans votre page HTML
+```html
+<link href='jquery.cute-element.css' rel='stylesheet' type='text/css'>
+```  
+
+### JS
+Importer le fichier dans votre page HTML après le script jQuery
+```html
+<script type="text/javascript" src="http://code.jquery.com/jquery-latest.min.js"></script>
+<script type="text/javascript" src="jquery.cute-element.min.js"></script>
+```  
+
+## Exemples
+```html
+<script type="text/javascript">
+    $('input[type="radio"], input[type="checkbox"]').cuteElement({
+        'theme': 'check-heavy'
+    });
+
+    $('select').cuteElement({
+        'selectWidth': '100px'
+    });
+</script>
+``` 
+
+## Paramètres du plugin jQuery
+
+#### labelPosition
+Position du label rapport à l'élément, avant ou après  
+Défaut: `next`
+
+#### theme
+Thèmes pour les éléments radio et checkbox, thèmes disponibles : checkbox, radio, check-heavy, bullet-outline, smiley-happy-face, vous pouvez créer de nouveaux thème grâce à la mixin Sass décrite ci-après ou en personnalisant le css  
+Défaut: `checkbox`
+
+#### selectWidth
+Largeur de l'élément select  
+Défaut: `auto`
+
+
+## Utilisation avancée avec Sass
+Plutôt que d'importer le fichier CSS vous pouvez inclure le fichier `jquery.cute-element.scss` dans votre projet, ainsi vous aurez accès à une mixin permettant de créer des thèmes pour vos élements radio et checkbox.
+
+```scss
+.my-custom-theme {
+    @include cute-element-input (
+        $border-radius: 20px,
+        $character: "\25E6",
+        $character-size: 50px,
+        $character-vertical-align: 0.17,
+        $character-horizontal-align: -4px,
+        $checked-color: #89B11C
+    );
+}
+``` 
+### Paramètres de la mixin
+
+#### $dimension
+Largeur et hauteur de l'élement en pixels  
+Défaut: `20px`
+
+#### $color
+Couleur de fond de de l'élément  
+Défaut: `#eee`
+
+#### $border-radius
+Taille des bordures arrondies de l'élément  
+Défaut: `0`
+
+#### $checked-dimension
+Largeur et hauteur du check  
+Défaut: `10px`
+
+#### $checked-color
+Couleur du check  
+Défaut: `#1290cc`
+
+#### $checked-border-radius
+Taille des bordures arrondies du check  
+Défaut: `0`
+
+#### $shadow
+Ombre de l'élément  
+Défaut: `0px 0px -3px -1px rgba(#000, 0.25)`
+
+#### $shadow-hover
+Ombre au survol  
+Défaut: `0px 0px -3px -1px rgba(#1290cc, 1)`
+
+#### $border
+Bordure de l'élément  
+Défaut: `1px solid #ddd`
+
+#### $character
+Si vous spécifiez ce paramètre vous utiliserez à la place du simple carré/rond un caractère HTML de votre choix  
+Défaut: `none`
+
+#### $character-size
+Taille du caractère  
+Défaut: `0`
+
+#### $character-vertical-align
+Ajustement vertical de la position du caractère en pixels ou en valeur décimal (line-height)  
+Défaut: `0`
+
+#### $character-horizontal-align
+Ajustement horizontal de la position du caractère en pixels (text-indent)  
+Défaut: `0`
---
+++ jquery.cute-element.js
@@ -0,0 +1,160 @@
+(function($) {
+    $.fn.cuteElement = function(options)
+    {
+       var defauts = {
+           "labelPosition": "next",
+           "theme": "checkbox",
+           "selectWidth": "auto",
+       };
+
+       var params = $.extend(defauts, options);
+
+        return this.each(function()
+        {
+            var element = $(this);
+            var tag = element.prop('tagName').toLowerCase();
+
+            switch(tag){
+                case 'select':
+
+                    var selected_option = element.find('option:selected');
+                    var icon = selected_option.data('icon');
+
+                    /* Largeur du select */
+                    var select_width = params.selectWidth;
+                    if(element.data('width')){
+                        select_width = element.data('width');
+                    }
+
+                    var selected_option_text = selected_option.text() != '' ? selected_option.text() : '&nbsp;';
+                    var a = $('<a href="#" class="cute-element-select cute-element-select-theme-' + params.theme + '">' + selected_option_text + '</a>').css('width', select_width);
+                    if(icon){
+                        a.css({
+                            'background-image': 'url("' + icon + '")'
+                        });
+                        a.addClass('cute-element-select-icon');
+                    }
+
+                    var ul = $('<ul class="cute-element-select-option"></ul>');
+                    element.find('option').each(function(){
+                        var icon = $(this).data('icon');
+                        var text = $(this).text() != '' ? $(this).text() : '&nbsp;';
+                        var a = $('<a href="#" data-value="' + $(this).attr('value') + '">' + text + '</a>');
+                        var li = $('<li></li>');
+                        li.append(a);
+                        ul.append(li);
+                        if(icon){
+                            a.css({
+                                'background-image': 'url("' + icon + '")'
+                            });
+                            a.addClass('cute-element-select-option-icon');
+                            a.data('icon', icon);
+                        }
+                    });
+
+                    element.after(a);
+                    $('body').append(ul);
+
+                    /* Taille minimum de la liste */
+                    var max_width = 0;
+                    ul.find('a').each(function(){
+                        if(max_width < a.width())
+                            max_width = a.width();
+                    });
+
+                    var padding = a.outerWidth() - a.width();
+
+                    ul.css({
+                        'min-width': max_width + padding
+                    });
+
+                    element.hide();
+
+                    /** CLic sur les options **/
+                    ul.find('a').click(function(event){
+                        event.preventDefault();
+                        ul.find('a').removeClass('selected');
+                        $(this).addClass('selected');
+                        element.find('option').prop('selected', false).attr('selected', false);
+                        element.find('option[value="' + $(this).data('value') + '"]').prop('selected', true).attr('selected', true);
+                        a.text($(this).text());
+                        var icon = $(this).data('icon');
+                        if(icon){
+                            a.css({
+                                'background-image': 'url("' + icon + '")'
+                            });
+                            a.addClass('cute-element-select-icon');
+                        }
+                        ul.toggleClass('cute-element-select-option-open');
+                        a.toggleClass('cute-element-select-open');
+                        element.trigger('change');
+                    });
+
+                    a.click(function(event){
+                        event.preventDefault();
+                        var top = $(this).offset().top + $(this).outerHeight();
+                        var left = $(this).offset().left;
+                        ul.css({'top': top + 'px', 'left': left + 'px'});
+                        ul.toggleClass('cute-element-select-option-open');
+                        $(this).toggleClass('cute-element-select-open');
+                    });
+                break;
+                case 'input':
+                    var type = element.attr('type');
+                    var a = $('<a href="#" class="cute-element-' + type + ' cute-element-input-theme-' + params.theme + '"></a>');
+
+                    if(params.labelPosition == 'next')
+                        var label = element.next('label');
+                    else
+                        var label = element.prev('label');
+
+                    if(label.length)
+                        label.addClass('cute-element-input-label');
+
+                    element.after(a);
+                    element.hide();
+
+                    if(element.is(':checked')){
+                        a.addClass('checked');
+                        label.addClass('checked');
+                    }
+
+                    if(element.prop('disabled')){
+                        a.addClass('disabled');
+                        a.click(function(event){
+                            event.preventDefault();
+                        });
+                        label.addClass('disabled');
+                    }
+                    else {
+                        a.click(function(event){
+                            event.preventDefault();
+                            switch(type){
+                                case 'checkbox':
+                                    if($(this).is(':checked'))
+                                        element.prop('checked', false).attr('checked', false);
+                                    else
+                                        element.prop('checked', true).attr('checked', true);
+                                    $(this).toggleClass('checked');
+                                break;
+                                case 'radio':
+                                    var name = element.attr('name');
+                                    $('input[type="radio"][name="' + name + '"]').prop('checked', false).attr('checked', false).each(function(){
+                                        $(this).next('a.cute-radio').removeClass('checked');
+                                    });
+                                    element.prop('checked', true).attr('checked', true);
+                                    $(this).addClass('checked');
+                                break;
+                            }
+                        });
+                        if(label.length)
+                            label.click(function(){
+                                a.trigger('click');
+                            });
+                    }
+
+                break;
+            }
+        });
+    };
+})(jQuery);
